08b624993ba6d0d602a6b0a4824cec6f62c9eaf5
Ícaro Costa <icarocmotta@gmail.com>
2020-01-30 17:30:06 -0300

    no comments

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,64 @@
 const mongoose = require('mongoose')
 const User = mongoose.model('User')
 
+savesUserData = (userData, req, res) => {
+
+  userData.save((err, userData) => {
+    if (err) {
+      res.status(404).json(err)
+    } else {
+      res.status(200).json(userData)
+    }
+  })
+}
+
+checkCart = (cart, itemId) => {
+
+  let checkResult = []
+  for (const item of cart) {
+    if (item.code === itemId) {
+      checkResult.push(item)
+    }
+  }
+
+  return checkResult
+}
+
+upSertItemToCart = (user, requisitionData, req, res) => {
+
+  const { productName, productId, amount } = requisitionData
+
+  let checkedCart = checkCart(user.cart, productId)
+
+  if (checkedCart.length > 0) {
+
+    user.cart.splice(user.cart.indexOf(checkedCart[0]), 1)
+    let modifiedItem = { name: productName, code: checkedCart[0].code, amount: amount }
+    user.cart.push(modifiedItem)
+    // TODO: DRY
+    user.save((err, user) => {
+      if (err) {
+        res.status(404).json(err)
+      } else {
+        let thisReview = user.cart[user.cart.length - 1];
+        res.status(200).json(thisReview)
+      }
+    })
+  } else {
+
+    let item = { name: productName, code: productId, amount: amount }
+    user.cart.push(item)
+    // TODO: DRY
+    user.save((err, user) => {
+      if (err) {
+        res.status(404).json(err)
+      } else {
+        res.status(200).json(user)
+      }
+    })
+  }
+}
+
 module.exports.register = (req, res) => {
 
   User.findOne({ email: req.body.email }, (err, user) => {
@@ -16,7 +74,7 @@ module.exports.register = (req, res) => {
     })
   })
 }
-/* Adiciona item ao carrinho*/
+
 module.exports.addItemToCart = (req, res) => {
 
   const { userId, productName, productCode, amount } = req.body;
@@ -66,116 +124,4 @@ module.exports.removeItemFromCart = (req, res) => {
       })
       // return res.send(user).status(200)
     });
-}
-
-/* Adiciona novo item */
-addItem = (item) => { }
-
-/* Verifica se o item existe no carrinho */
-checkCart = (cart, itemId) => {
-
-  let checkResult = []
-  for (const item of cart) {
-    if (item.code === itemId) {
-      checkResult.push(item)
-    }
-  }
-
-  return checkResult
-}
-
-/* Verifica a disponibilidade do item enviado */
-upSertItemToCart = (user, requisitionData, req, res) => {
-
-  const { productName, productId, amount } = requisitionData
-  let checkedCart = checkCart(user.cart, productId)
-
-  // TODO: Adiciona bloco de código ao metodo addItem
-  if (checkedCart.length > 0) {
-
-    user.cart.splice(user.cart.indexOf(checkedCart[0]), 1)
-    let modifiedItem = { name: productName, code: checkedCart[0].code, amount: amount }
-    user.cart.push(modifiedItem)
-
-    user.save((err, user) => {
-      if (err) {
-
-        res.status(404).json(err)
-      } else {
-        let thisReview = user.cart[user.cart.length - 1];
-        res.status(200).json(thisReview)
-      }
-    })
-  } else {
-
-    console.log('INSERIR')
-
-    // TODO: Adiciona bloco de código ao metodo addItem
-    let item = { name: productName, code: productId, amount: amount }
-    user.cart.push(item)
-
-    user.save((err, user) => {
-      if (err) {
-        res.status(404).json(err)
-      } else {
-        res.status(200).json(user)
-      }
-    })
-  }
-
-
-  // for (const item of cart) {
-
-  // console.log(item);
-
-  // if(!item) {
-  //   console.log('Salva!', item);
-  // } else {
-  //   console.log('Atualiza', item);
-  // }
-
-  // user.cart.splice(user.cart.indexOf(item.code), 1);
-  // let updateItem = { name: productName, code: productCode, amount: amount }
-  // user.cart.push(updateItem)
-  // console.log('>>', user)
-
-
-  // Atualiza a quantidade do item(produto)
-  // if (item.code === productCode) {
-
-  // console.log('>>', item)
-
-  // user.cart.splice(user.cart.indexOf(item.code), 1);
-  // let updateItem = { name: productName, code: productCode, amount: amount }
-  // user.cart.push(updateItem)
-
-
-
-  // user.save((err, user) => {
-  //   if (err) {
-
-  //     res.status(404).json(err)
-  //   } else {
-  //     let thisReview = user.cart[user.cart.length - 1];
-  //     res.status(200).json(thisReview)
-  //   }
-  // })
-
-
-  // } else {
-  // console.log('>>>>>')
-  // Adiciona novo item
-  // let newItem = { name: productName, code: productCode, amount: amount }
-  // user.cart.push(newItem)
-  // user.save((err, user) => {
-  //   if (err) {
-  //     res.status(404).json(err)
-  //   } else {
-  //     let thisReview = user.cart[user.cart.length - 1];
-  //     res.status(200).json(thisReview)
-  //   }
-  // });
-  // }
-
-  // }
 }
